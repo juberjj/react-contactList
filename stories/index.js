713c7f6549787b58c6e48c67b2c7dcb428c8e811
Juber Nunes <jnunes@localhost.localdomain>
2019-07-07 13:31:56 +0100

    Contact component completed

--- stories/index.js
+++ stories/index.js
@@ -4,6 +4,15 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 import Header from '../src/components/header/'
 import FilterControls from '../src/components/filterControls/'
+import Contact from '../src/components/contact/'
+
+const sample = {
+  name: {first:'Joe', last:'Bloggs'},
+  email: 'j.bloggs@example.com',
+  phone: '012-3456789',
+  picture: {thumbnail: './profile.png'}
+}
+
 
 storiesOf("Contact List App/Header", module).add("default", () => (
   <Header noContacts={10} />
@@ -13,3 +22,7 @@ storiesOf("Contact List App/Header", module).add("default", () => (
 storiesOf("Contact List App/Filter Controls", module).add("default", () => (
   <FilterControls />
 ));
+
+storiesOf("Contact List App/Contact", module).add("default", () => ( 
+    <Contact contact={sample}/>
+  ));
