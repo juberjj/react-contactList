362f344f53cf19613a3e3709b29e9eb3e0a79873
juberjj <juberjj@yahoo.ie>
2019-07-08 20:25:54 +0100

    Routing setup completed

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import { MemoryRouter, Route } from "react-router";
 
 import Header from '../src/components/header/'
 import FilterControls from '../src/components/filterControls/'
@@ -10,9 +11,41 @@ import { action } from '@storybook/addon-actions';
 
 const sample = {
   name: {first:'Joe', last:'Bloggs'},
+  location: {
+    street: "ljan terrasse 346",
+    city: "vear",
+    state: "rogaland",
+    postcode: "3095",
+    coordinates: {
+      latitude: "54.8646",
+      longitude: "-97.3136"
+    },
+    timezone: {
+      offse: "-10:00",
+      description: "Hawaii"
+    }
+  },
+    "login": {
+      "uuid": "c4168eac-84b8-46ea-b735-c9da9bfb97fd",
+      "username": "bluefrog786",
+      "password": "ingrid",
+      "salt": "GtRFz4NE",
+      "md5": "5c581c5748fc8c35bd7f16eac9efbb55",
+      "sha1": "c3feb8887abed9ec1561b9aa2c9f58de21d1d3d9",
+      "sha256": "684c478a98b43f1ef1703b35b8bbf61b27dbc93d52acd515e141e97e04447712"
+    },
+    "dob": {
+      "date": "1975-11-12T06:34:44Z",
+      "age": 42
+    },
+    "registered": {
+      "date": "2015-11-04T22:09:36Z",
+      "age": 2
+    },
   email: 'j.bloggs@example.com',
   phone: '012-3456789',
-  picture: {thumbnail: './profile.png'}
+  picture: {thumbnail: './profile.png',
+  large: './profile.png'}
 }
 
 
@@ -25,11 +58,19 @@ storiesOf("Contact List App/Filter Controls", module).add("default", () => (
   <FilterControls />
 ));
 
-storiesOf("Contact List App/Contact", module).add("default", () => ( 
-    <Contact contact={sample}/>
-  ));
-  
-storiesOf("Contact List App/Contact List", module).add("default", () => { 
+storiesOf("Contact List App/Contact", module)
+.addDecorator(story => (
+  <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+))
+.add("default", () => ( 
+   <Contact contact={sample} deleteHandler={action('Delete confirmed') }/>
+));
+
+storiesOf("Contact List App/Contact List", module)
+.addDecorator(story => (
+  <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+))
+.add("default", () => { 
   const samples = [sample, sample, sample, sample, sample]
   return <ContactList contacts={samples}/>
 });
