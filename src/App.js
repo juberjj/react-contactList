13b7591ce8f88983d072a6d40f282b177479816a
juberjj <juberjj@yahoo.ie>
2019-07-09 07:59:57 +0100

    Stub contact page working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React, { Component } from "react";
 import Header from "./components/header/";
 import ContactList from "./components/contactList/";
 import FilterControls from "./components/filterControls/";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import request from "superagent";
+//import request from "superagent";
 import api from "./dataStore/stubAPI"
 import _ from 'lodash'
+import React, { Component, Fragment } from "react";
 
 class App extends Component {
   state = { search: "", gender: "all" };
-componentDidMount() {
-  request.get("https://randomuser.me/api/?results=50").end((error, res) => {
-  if (res) {
-      let { results: contacts } = JSON.parse(res.text);
-      api.initialize(contacts);
-      this.setState({});
-  } else {
-      console.log(error);
-  }
-  });
-}
 
 handleChange = (type, value) => {
   type === "name"
@@ -46,14 +35,14 @@ render(){
   let sortedContacts = _.sortBy(filteredContacts, c => c.name.last);
 
   return (
-  <div className="jumbotron">
-      <Header noContacts={sortedContacts.length} />
-      <FilterControls
-          onUserInput={this.handleChange}
-      />
-      <ContactList contacts={sortedContacts}
-      deleteHandler={this.deleteContact}  />
-  </div>
+    <Fragment>
+    <Header noContacts={sortedContacts.length} />
+    <FilterControls onUserInput={this.handleChange} />
+    <ContactList
+      contacts={sortedContacts}
+      deleteHandler={this.deleteContact}
+    />
+  </Fragment>
   );
 }
 }
